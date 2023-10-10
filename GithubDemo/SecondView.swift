65f4cc60a4413819960b31dcc6b1edc6d123a4af
dnstrach <dnstrach@gmail.com>
2023-10-10 08:53:38 -0700

    create second view

---
+++ GithubDemo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GithubDemo
+//
+//  Created by Dominique Strachan on 10/10/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
