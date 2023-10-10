1ca112b52e406e9eab6e01b78689ac7d9cc705ab
dnstrach <dnstrach@gmail.com>
2023-10-10 08:42:44 -0700

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Dominique Strachan on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
